35b80ff871c3f1342670aa2912a0bbaf476c6385
Matthew Locke <mlocke@soundwebsupport.com>
2011-10-29 16:06:03 -0700

    Validation!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '19e1f9180a6c6b49d4468a43b9538b499f4db494c76d6b0fa882bfd7362349f3828a6d8b9ba0652d94b072eb7cd60e58765e1f0188c6502c9acf5c4a93025277'
